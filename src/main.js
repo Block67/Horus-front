f58004885155d106914851eb77f703b7ff795df7
Rahamane BODA <block6703@gmail.com>
2023-08-19 15:16:44 +0100

    ajout des pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
+import router from './routes';
 import '../src/assets/css/output.css'
 import '../src/assets/css/input.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+createApp(App)
+    .use(router)
+    .mount('#app');
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Dashboard from '../views/Dashboard.vue';
-import Contact from '../views/Contact.vue';
-import Connexion from '../views/Connexion.vue';
-import About from '../views/About.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/dashboard", name: "dashboard", component: Dashboard },
-  { path: "/connexion", name: "connexion", component: Connexion },
-  { path: "/about", name: "about", component: About },
-  { path: "/contact", name: "contact", component: Contact },
 
 
 
